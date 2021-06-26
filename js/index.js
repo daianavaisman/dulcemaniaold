030f11c155887827f58395079bc0c32b3695c2b5
dvaisman <daiana.vaisman@gmail.com>
2021-06-26 19:14:19 -0300

    html edits

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const products = [
     { id: 1, title:"Tremendo Choco",           price: 500, image: "images/ch1.jpg", altText: "Choco 1", description: "Centro de magia y cobertura de locura.", buttonText: "Lo Quiero!"},
     { id: 2, title:"Bomba de Amor",            price: 550, image: "images/ch2.jpg", altText: "Choco 2", description: "Una explosión de sabores, sabelo.",      buttonText: "Lo Necesito!"},
-    { id: 3, title:"Una Fiesta",               price: 525, image: "images/ch3.png", altText: "Choco 3", description: "No podés creer que algo así exista!",    buttonText: "Dame Mil!"},
+    { id: 3, title:"Una Fiesta",               price: 525, image: "images/ch3.jpg", altText: "Choco 3", description: "No podés creer que algo así exista!",    buttonText: "Dame Mil!"},
     { id: 4, title:"Festival de Sabores",      price: 500, image: "images/ch4.jpg", altText: "Choco 4", description: "Te vas a querer casar con este choco.",  buttonText: "Ufff.. Compro!"},
     { id: 5, title:"Esto es Fatal",            price: 550, image: "images/ch5.jpg", altText: "Choco 5", description: "Todo lo que está bien (y mal)...",       buttonText: "Tome mi Dinero!"},
     { id: 6, title:"El Chocolate de tu Vida",  price: 525, image: "images/ch6.jpg", altText: "Choco 6", description: "No existe otro como este, he dicho!",    buttonText: "Lo Deseo!"}
@@ -26,12 +26,36 @@ productsContainer.innerHTML =
         <h2>${product.title}</h2>
         <p class="clases-text-responsive">${product.description}</p>
         <p class="clases-text-responsive"><img src="images/precio.png" alt="Precio"> ${product.price}</p> 
-        <button type="button" class="btn addToCartButton">${product.buttonText}</button>
+        <button type="button" value="${product.id}" class="btn addToCartButton">${product.buttonText}</button>
     </div>
  </div>`
 
 container.appendChild(productsContainer)
 }
+
+// FUNCIONES Y EVENTOS PARA AGREGAR AL CARRITO - INICIO
+const cart = []
+
+function addToCart(e) {
+    //.find() es un metodo de los arrays que te devuelve el elemento que coincida con la busqueda. 
+    //en este caso buscamos que me traiga el product.id que coincida con el value del boton
+    const productToCart = products.find(product => product.id === parseInt(e.target.value));//o e.value prueba con ambas a ver cual funciona
+    console.log(productToCart);
+
+    if(!productToCart) {
+        console.log('hmm no consigo el producto');
+        return;
+    }
+    cart.push(productToCart);
+    saveToLocalStorage("cart", cart)
+}
+
+
+const cartButton = document.querySelectorAll(".addToCartButton");
+cartButton.forEach(button => button.addEventListener("click", addToCart)) //la función está definida arriba
+
+
+// FUNCIONES Y EVENTOS PARA AGREGAR AL CARRITO - FIN
 
 
 
